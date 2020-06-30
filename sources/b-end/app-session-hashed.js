78a1299241efe6577b7936e4d7ae30bff6543bdb
Withered Flowers <withered-flowers@users.noreply.github.com>
2020-06-30 10:16:38 +0700

    feat: fallback to bcryptjs for better compat

--- sources/b-end/app-session-hashed.js
+++ sources/b-end/app-session-hashed.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const session = require('express-session');
 // Import bcrypt
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 // Import Reals
 const { User, Real } = require('./models/index.js');
